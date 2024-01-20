368654884ad77eefa8d4b109515925966dc21718
Alok Nayan <aloknayan@Aloks-MacBook-Air.local>
2024-01-21 00:45:18 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(username==="user" && password==="password"){
+      setMessage("Welcome, user!");
+    }else{
+      setMessage("Invalid username or password");
+    }
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="inputField">
+          <label>
+            Username:
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </label>
+        </div>
+        <div className="inputField">
+          <label>
+            Password:
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+        </div>
+        <button className="submitBtn">Submit</button>
+      </form>
+      <p>{message}</p>
     </div>
   );
 }
